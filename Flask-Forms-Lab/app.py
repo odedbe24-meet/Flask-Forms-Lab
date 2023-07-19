9a46c57c30e8771a67ed4edcad56f101c4482517
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-19 11:18:02 +0300

    Finished Everything Including The Bonus

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -10,13 +10,43 @@ app = Flask(  # Create a flask app
 
 username = "llo2ay"
 password = "123"
+
+accounts = {"llo2ay":"123", "oded":"567"}
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		user = request.form['username']
+		passw= request.form['password']
+
+		for i in accounts:
+
+			if(user == i and passw == accounts[i]):
+				return redirect(url_for('home'))
+
+		else:
+			return render_template('login.html')	
+
+@app.route('/home')
+def home():
+	return render_template('home.html'
+			,fl=facebook_friends)
+
+
+
+@app.route('/friend_exists/<string:friend>', methods=['GET', 'POST'])
+def friend_exists(friend):
+	if(friend in facebook_friends):
+		return render_template('friend_exists.html', friend=friend,b="they are!")
+	else:
+		return render_template('friend_exists.html', friend=friend, b="they are not!")
+	
+
+
 
 
 
